51116970d6ba1568b7b508522ae1c87556916e6e
Manish <manishboge@gmail.com>
2022-04-24 09:31:03 +0530

    feat: added ng-content  usecases

--- src/app/ng-container-template/ng-container-template.module.ts
+++ src/app/ng-container-template/ng-container-template.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgCotainerDemoComponent } from './ng-cotainer-demo/ng-cotainer-demo.component';
 import { NgContainerTemplateRoutingModule } from './ng-container-template-routing.module';
+import { NgContentDemoComponent } from './ng-content-demo/ng-content-demo.component';
+import { NgTemplateDemoComponent } from './ng-template-demo/ng-template-demo.component';
 
 
 
 @NgModule({
   declarations: [
-    NgCotainerDemoComponent
+    NgCotainerDemoComponent,
+    NgContentDemoComponent,
+    NgTemplateDemoComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/ng-container-template/ng-template-demo/ng-template-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-template-demo',
+  templateUrl: './ng-template-demo.component.html',
+  styleUrls: ['./ng-template-demo.component.scss']
+})
+export class NgTemplateDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
